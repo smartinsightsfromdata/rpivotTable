eb53b9c188a14084fc79418b102abb67ecd63b59
timelyportfolio <kent.russell@timelyportfolio.com>
2015-02-05 22:46:35 -0600

    add d3 dependencies and renderers

--- inst/htmlwidgets/rpivotTable.js
+++ inst/htmlwidgets/rpivotTable.js
@@ -15,11 +15,12 @@ HTMLWidgets.widget({
 
 	    var derivers = $.pivotUtilities.derivers;
       var tpl = $.pivotUtilities.aggregatorTemplates;
-      /* remove d3 for now to simplify for testing
-           will add it back once the widget if fleshed out and tested
-      x.params.renderers = $.extend($.pivotUtilities.renderers),
-        $.pivotUtilities.d3_renderers);
-      */
+
+      x.params.renderers = $.extend(
+        $.pivotUtilities.renderers,
+        $.pivotUtilities.d3_renderers
+      );
+
 
       $('#'+el.id).pivotUI(
       		x.data, x.params
