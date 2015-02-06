32561fddc66780ae649c08a22160d838eaacb038
timelyportfolio <kent.russell@timelyportfolio.com>
2015-02-06 11:50:18 -0600

    add tests of rpivotTable

---
+++ tests/testthat/test_data.R
@@ -0,0 +1,20 @@
+context("Data")
+
+test_that("data is checked",{
+  expect_error(rpivotTable( data= NULL ) )
+  expect_error(rpivotTable( 1:20 ))
+  expect_error(rpivotTable( matrix(1:20,ncol=2) ))
+})
+
+test_that("data is converted",{
+  expect_is(rpivotTable( Titanic )$x$data,  "data.frame")
+  expect_warning(rpivotTable( data.table(iris) ))
+  expect_is(suppressWarnings(rpivotTable( data.table(iris) ))$x$data, "data.frame")
+  expect_is(rpivotTable( data.frame(x=1:10,y=1:10) )$x$data, "data.frame")
+})
+
+test_that("data is not changed",{
+  expect_identical(
+    rpivotTable( data.frame(x=1:10,y=LETTERS[1:10]) )$x$data
+    , data.frame(x=1:10,y=LETTERS[1:10]))
+})
