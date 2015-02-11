6f04935f384fa1fec04d058b988c9f9ae9e91b22
“smartinsightsfromdata” <enzo@smartinsightsfromdata.com>
2015-02-11 09:27:06 +0000

    Version: 0.1.2.7.2 - data for examples repositioned in /data

--- inst/examples/canElections-shiny.R
+++ inst/examples/canElections-shiny.R
@@ -4,7 +4,7 @@ library(shiny)
 
 # canEldt <- fread(paste0("./inst/examples/", "canadaFctElections.csv"),header=T)
 # save(canEldt,file="./inst/examples/canEldt.RData")
-load(file="./inst/examples/canEldt.RData")
+load(file="data/canEldt.RData")
 
 canEldt$votes <- round(runif(nrow(canEldt), min=500, max=15000))
 
--- inst/examples/canElections.R
+++ inst/examples/canElections.R
@@ -3,7 +3,7 @@ library(data.table)
 
 # canEldt <- fread(paste0("./inst/examples/", "canadaFctElections.csv"),header=T)
 # save(canEldt,file="./inst/examples/canEldt.RData")
-load(file="./inst/examples/canEldt.RData")
+load(file="data/canEldt.RData")
 
 canEldt$votes <- round(runif(nrow(canEldt), min=500, max=15000))
 
