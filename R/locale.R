16a47388c5806270934c18f32614889252d21bfc
Nicolas Kruchten <nicolas@datacratic.com>
2018-01-24 21:43:32 -0500

    update to 2.19.0, fix locales

--- R/locale.R
+++ R/locale.R
@@ -1,5 +1,5 @@
 #' Localization
-#' 
+#'
 #' Change localization for \code{rpivotTable} to support non-English language.
 #' For more discussion, see
 #' \href{https://github.com/nicolaskruchten/pivottable/wiki/Localization}{pivottable Localization}.
@@ -13,7 +13,7 @@
 #'            \item{pt}
 #'            \item{tr}
 #'         }
-#'         
+#'
 #'
 #' @return \code{rpivotTable} object
 #' @export
@@ -26,21 +26,8 @@ change_locale <- function(
   pivottable = NULL, locale = NULL
 ) {
   if(!inherits(pivottable,"rpivotTable")) stop("pivottable should be a rpivotTable.",call.=F)
-  
-  locale_dep <- htmltools::htmlDependency(
-    name = sprintf("pivottable-%s",locale)
-    ,version = "1.6.3"
-    ,src = c(file=system.file("htmlwidgets/lib/pivottable/dist",package="rpivotTable"))
-    ,script = sprintf("pivot.%s.min.js",locale)
-  )
-  
-  if(length(pivottable$dependencies) == 0){
-    pivottable$dependencies = list()
-  }
-  
-  pivottable$dependencies[[length(pivottable$dependencies)+1]] <- locale_dep
-  
+
   pivottable$x$locale <- locale
-  
+
   return(pivottable)
 }
